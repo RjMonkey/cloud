c7b06e884de82b4fbee2f2b0e493890a705d8ccb
programmingduo <wuduotju@163.com>
2016-10-16 18:36:59 +0800

    new directory

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'TMPL_VAR_IDENTIFY' => 'array',
+    'DEFAULT_FILTER'        =>  'strip_tags,stripslashes,htmlspecialchars',
+    "SHOW_PAGE_TRACE"=>true,  //可以实时显示当前页面的操作的请求信息、运行情况、SQL执行、错误提示等，并支持自定义显示
+    "SHOW_ERROR_MSG"=>true,       //显示具体的错误信息了
 );
---
+++ Application/Home/Common/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuduo
+ * Date: 16-10-16
+ * Time: 下午6:28
+ */
