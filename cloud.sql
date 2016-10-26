9061ac519f1fe44a8227dea223d7d5e893f9a4e9
future <15522962389@163.com>
2016-10-26 21:12:44 +0800

    数据库

---
+++ cloud.sql
@@ -0,0 +1,76 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1_3306
+Source Server Version : 50713
+Source Host           : 127.0.0.1:3306
+Source Database       : cloud
+
+Target Server Type    : MYSQL
+Target Server Version : 50713
+File Encoding         : 65001
+
+Date: 2016-10-26 21:11:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cloud_login
+-- ----------------------------
+DROP TABLE IF EXISTS `cloud_login`;
+CREATE TABLE `cloud_login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `login_ip` varchar(255) NOT NULL,
+  `login_time` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cloud_login
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cloud_upload
+-- ----------------------------
+DROP TABLE IF EXISTS `cloud_upload`;
+CREATE TABLE `cloud_upload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `identity` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `upload_time` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `is_delete` varchar(255) DEFAULT NULL,
+  `is_examine` varchar(255) DEFAULT NULL,
+  `group` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cloud_upload
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cloud_vote
+-- ----------------------------
+DROP TABLE IF EXISTS `cloud_vote`;
+CREATE TABLE `cloud_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `art_id` int(11) NOT NULL,
+  `vote_num` varchar(255) DEFAULT NULL,
+  `vote_time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cloud_vote
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS=1;
