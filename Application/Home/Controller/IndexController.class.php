4be2aa5e4ecebab558ea09bb34898a4829cbe557
RjMonkey <15522962389@163.com>
2017-06-01 11:35:12 +0800

    new

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,25 +9,17 @@ class IndexController extends Controller {
      */
     public function index()
     {
-<<<<<<< HEAD
 //        echo md5("admin");
-=======
->>>>>>> 0317c2698a776a5de4ca6aaab2d23ec10f2c178a
         $this->show();
     }
 
-    /**
-     *
-     */
     public function login()
     {
         // 判断提交方式
         if (IS_POST) {
-
             $username = trim(I('post.username'));
             $userpsd = trim(I('post.password'));
 
-<<<<<<< HEAD
             $result = M("User");
             $fwhere['username'] = $username;
             $fwhere['password'] = md5($userpsd);
@@ -45,45 +37,14 @@ class IndexController extends Controller {
             {
                 echo "<script>alert('账户密码输入错误！');javascript:window.location.href='".U('Index/index')."'</script>";
             }
-
-        } else {
-
-=======
-            $result = M("lms_user", null);
-            $fwhere['user_number'] = $username;
-            $fwhere['user_password'] = $userpsd;
-            $find = $result->where($fwhere)->select();
-
-            if(I('session.username') || $find)
-            {
-                $userid = $find[0]['id'];
-                session('username', 1);
-                session('userid', $userid);
-//                $this->redirect('Personal/personal');
-                echo "<script language='javascript'>alert('登录成功！');javascript:window.location.href='".U('Personal/personal');
-            }
-            else
-            {
-//                $this->error("帐号或密码输入错误");
-                echo "<script language='javascript'>alert('账户密码输入错误！');javascript:history.go(-1);</script>";
-//                echo "<script></script>";
-            }
-
-        } else {
-            //echo "inelse";
->>>>>>> 0317c2698a776a5de4ca6aaab2d23ec10f2c178a
-            $this->display();
         }
+        $this->display();
     }
 
     public function logout()
     {
         session(null);
-<<<<<<< HEAD
         echo "<script language='javascript'>alert('注销成功！');javascript:window.location.href='".U('Index/index');
-=======
-        $this->success("注销成功");
->>>>>>> 0317c2698a776a5de4ca6aaab2d23ec10f2c178a
     }
 
 }
